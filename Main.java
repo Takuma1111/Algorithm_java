6d976d2fbbade9bd33ab106fe1ad9847f7c87630
Takuma <takuma012011@gmail.com>
2020-05-15 16:34:14 +0900

    BinarySearchTree add

--- Main.java
+++ Main.java
@@ -2,8 +2,10 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		
+		//配列の中に渡した値があれば二分木探索でその配列の番地を返す
+		BinarySearchTree tree = new BinarySearchTree();
+		System.out.println(tree.binarysearch(14));
 	}
 
 }
